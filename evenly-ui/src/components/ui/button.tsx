fddb912a94e044194994f69f8fa0ed35aeeeecf1
Priyak Dey <priyak1991dey@gmail.com>
2025-06-01 23:58:04 +0530

    integrate ui with the profile api

--- evenly-ui/src/components/ui/button.tsx
+++ evenly-ui/src/components/ui/button.tsx
@@ -1,6 +1,3 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-// Copyright (C) 2025 Priyak Dey
-
 // SPDX-License-Identifier: MIT
 // This file is copied from shadcn/ui (https://github.com/shadcn-ui/ui)
 // License: MIT
